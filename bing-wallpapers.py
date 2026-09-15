bfbdb7d78c9cca3b8107c07d981810c2f9b34af2
Mohammad Hassan Sohan Ajini <mohamad.hasan.sohan.ajini@gmail.com>
2026-09-15 18:16:20 +0100

    Improve image name

--- bing-wallpapers.py
+++ bing-wallpapers.py
@@ -2,6 +2,7 @@
 import json
 import subprocess
 from pathlib import Path
+from urllib.parse import parse_qs, urlsplit
 
 import requests
 
@@ -14,9 +15,12 @@ CMD = 'gsettings set org.gnome.desktop.background picture-uri "file://{}"'
 def check_new_image():
     r = requests.get(BING)
     j = json.loads(r.text)
-    urlbase = j["images"][0]["urlbase"]
+    image_info = j["images"][0]
+    urlbase = image_info["urlbase"]
     image_url = f"https://bing.com{urlbase}_1920x1080.jpg"
-    filename = image_url.split("/")[-1]
+    image_id = parse_qs(urlsplit(image_url).query)["id"][0]
+    image_name = Path(image_id).name.removeprefix("OHR.")
+    filename = f"{image_info['startdate']}_{image_name}"
 
     image_path = BASE_DIR / filename
     if not image_path.exists():
