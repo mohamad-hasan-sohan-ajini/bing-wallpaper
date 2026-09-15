dba5c3422cd7cd0cbfbee18931d44207f157afc7
Mohammad Hassan Sohan Ajini <mohamad.hasan.sohan.ajini@gmail.com>
2026-09-15 18:11:21 +0100

    Amend the saving path

--- bing-wallpapers.py
+++ bing-wallpapers.py
@@ -1,30 +1,30 @@
 # coding: utf-8
-import os
-import subprocess
-import requests
 import json
-import pwd
+import subprocess
+from pathlib import Path
+
+import requests
 
 # CONSTANT
-BASE_DIR = f'/home/{pwd.getpwuid(os.geteuid()).pw_name}/Pictures/bing-wallpaper'
-BING = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
+BASE_DIR = Path.home() / "Pictures" / "bing-wallpaper" / "bing-wallpapers"
+BING = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
 CMD = 'gsettings set org.gnome.desktop.background picture-uri "file://{}"'
 
 
 def check_new_image():
     r = requests.get(BING)
     j = json.loads(r.text)
-    urlbase = j['images'][0]['urlbase']
+    urlbase = j["images"][0]["urlbase"]
     image_url = f"https://bing.com{urlbase}_1920x1080.jpg"
-    filename = image_url.split('/')[-1]
+    filename = image_url.split("/")[-1]
 
-    if filename not in os.listdir(BASE_DIR):
+    image_path = BASE_DIR / filename
+    if not image_path.exists():
         image = requests.get(image_url)
-        with open(os.path.join(BASE_DIR, filename), 'wb+') as f:
+        with image_path.open("wb+") as f:
             f.write(image.content)
-        image_path = os.path.abspath(os.path.join(BASE_DIR, filename))
-        subprocess.Popen(CMD.format(image_path), shell=True)
+        subprocess.Popen(CMD.format(image_path.resolve()), shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_new_image()
